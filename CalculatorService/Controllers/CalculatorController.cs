2b2591bf0ae30386724937aefe2e58d05527cfb0
CarlosNic <carlosdnicolau@outlook.com>
2024-01-12 22:41:33 -0300

    CalculatorController Implemented

---
+++ CalculatorService/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet(Name = "GetCalcute")]
+        public string Calculate(string str)
+        {
+            CalculatorConsoleApp.CalculatorClass calculator = new CalculatorConsoleApp.CalculatorClass();
+            return calculator.Calculate(str);
+        }
+    }
+}
